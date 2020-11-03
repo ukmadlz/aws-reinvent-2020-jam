42bea79759196da09d78b1f00b13548443af2e1f
Mike Elsmore <mike.elsmore@logz.io>
2020-11-03 10:51:10 +0000

    Change the metric generator script

--- metricsGen.sh
+++ metricsGen.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-
-# Consts
-BLOCKS=35000000
-RAND=$(shuf -i 1-10 -n 1)
-
-# How many times it'll run
-COUNTEDBLOCKS=$(( $BLOCKS ))
-echo "Process count: " $COUNTEDBLOCKS
-
-# Blast the CPU
-dd if=/dev/zero of=/dev/null count=$COUNTEDBLOCKS;
+while :
+do
+  dd if=/dev/zero of=/dev/null;
+  sleep 1
+done
